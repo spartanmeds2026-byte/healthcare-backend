d25bd7047e7f3861b1c1fd6a97903eb636190f23
spartanmeds2026-byte <spartanmeds2026@gmail.com>
2025-12-04 14:27:23 -0400

    Cors

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ export const createApp = ({
   app.use(cookieParser())
 
   // CORS
-  const allowedOrigins = ['http://localhost:5173', '*.infraxio.app', '*.medstreamhealthcare.com']
+  const allowedOrigins = ['http://localhost:5173', '*.infraxio.app', '*.vercel.app',  '*.medstreamhealthcare.com']
   const corsOptions = {
     origin: function (origin, callback) {
       if (!origin) return callback(null, true) // Allow requests with no origin (like mobile apps or curl requests)
